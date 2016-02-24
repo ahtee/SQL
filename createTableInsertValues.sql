c6678420279924501bc4728d58fdb11e5386c614
Ben Otte <bencotte@icloud.com>
2016-02-23 21:28:05 -0500

    Create createTableInsertValues.sql

---
+++ createTableInsertValues.sql
@@ -0,0 +1,12 @@
+create table otteb1_customer (customerID char(3) primary key, customerAddress char(25), customerName char(25),
+	customerCity Char(10), customerState char(2), orderID char(3) primary key );
+
+Insert into otteb1_customer Values ('22','532 Blue Road', 'Toms Hardware', 'Cincinnati', 'OH', '100'); 
+
+Insert into otteb1_customer Values ('50','3642 Biltmor Av', 'Anandtech', 'Tallahassee', 'FL', '104');
+
+Insert into otteb1_customer Values ('81','5122 Alvin Hei', 'Sharons Thrift', 'Austin', 'TX', '33');
+
+Insert into otteb1_customer values ('33', '743 Evergreen', 'Widget Resell', 'Springfield', 'MO', '107');
+
+insert into otteb1_customer values ('22', '532 Blue Road', 'Toms Hardware', 'Cincy ', 'OH', '39');
