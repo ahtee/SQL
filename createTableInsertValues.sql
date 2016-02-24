eb0d0c6d806467c76aad284ad4af98d332e13548
Ben Otte <bencotte@icloud.com>
2016-02-23 21:29:43 -0500

    Update createTableInsertValues.sql

--- createTableInsertValues.sql
+++ createTableInsertValues.sql
@@ -10,3 +10,21 @@ Insert into otteb1_customer Values ('81','5122 Alvin Hei', 'Sharons Thrift', 'Au
 Insert into otteb1_customer values ('33', '743 Evergreen', 'Widget Resell', 'Springfield', 'MO', '107');
 
 insert into otteb1_customer values ('22', '532 Blue Road', 'Toms Hardware', 'Cincy ', 'OH', '39');
+
+<!-------------- -->
+CREATE TABLE otteb1_otteb1_midterm (ASSIGN_NAME CHAR(40),
+	GRADE NUMBER(5,2), POINT_TOTAL NUMBER(5,2) );
+
+INSERT INTO otteb1_midterm VALUES ('Design Project 8/30 (G)', 10.00, 10);
+INSERT INTO otteb1_midterm VALUES ('QUIZ 1', 26.00, 40);
+INSERT INTO otteb1_midterm VALUES ('ERD Individual (9-11)', 7.50, 10);
+INSERT INTO otteb1_midterm VALUES ('Group Assignment 9-13', 10.00, 10);
+
+INSERT INTO otteb1_midterm VALUES ('Chapter 3-R.O.', 8.50, 10);
+INSERT INTO otteb1_midterm VALUES ('Chapt 3 - Integrity', 9.50, 10);
+INSERT INTO otteb1_midterm VALUES ('CH 3 Quiz', 66.6666, 100);
+INSERT INTO otteb1_midterm VALUES ('CH 10 In-class individ', 6.50, 10.00);
+
+INSERT INTO otteb1_midterm VALUES ('Ch 4 (G)', 10.00, 10.00);
+
+SELECT SUM(GRADE) FROM otteb1_midterm;
